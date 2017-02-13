dfc3c0a5906c046c5d86810867b68ab9c2926cc5
Princess-Jewel Essien <retrostereoyak@gmail.com>
2017-02-13 17:26:50 +0100

    Complete implementation of getPrimes

--- src/getprimes.js
+++ src/getprimes.js
@@ -1,23 +1,31 @@
 'use strict'
 
 module.exports = {
+    // This function generates via asymptotic analysis the primes between 0 and num (inclusive)
     getPrimes: function(num) {
+        // Return 'Not an integer' if num is not an integer
         if (!Number.isInteger(num)) {
             return 'Not an integer';
         }
 
+        // Return -1 if num is negative
         if (num < 0) {
             return -1;
         }
 
+        // Return undefined if num is zero
         if (num === 0) {
             return;
         }
 
+        // Return empty array if there are no primes between 0 and num
         if (num === 1) {
             return [];
         }
 
+        // Return array of primes between 0 and num
+        // Modified Sieve of Eratosthenes that ignores even numbers from the start
+        // Base algorithm found at http://en.wikipedia.org/wiki/Sieve_of_Eratosthenes
         let sieve = [];
         let result = [];
 
