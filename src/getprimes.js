e4a96c98cdd7420690a3d104e91dc789bfef4a24
Princess-Jewel Essien <retrostereoyak@gmail.com>
2017-02-13 17:22:12 +0100

    Implement case for valid values

--- src/getprimes.js
+++ src/getprimes.js
@@ -13,5 +13,30 @@ module.exports = {
         if (num === 0) {
             return;
         }
+
+        if (num === 1) {
+            return [];
+        }
+
+        let sieve = [];
+        let result = [];
+
+        for (var a = 0; a <= num / 2; a++) {
+            sieve.push(true);
+        }
+        
+        result.push(2);
+        
+        for (var i = 3; i <= num; i += 2) {
+            if (sieve[~~(i/2)]) {
+                result.push(i);
+                
+                for (var j = i*i; j <= num; j += 2*i) {
+                    sieve[~~(j/2)] = false;
+                }
+            }
+        }
+
+        return result;
     }
 }
